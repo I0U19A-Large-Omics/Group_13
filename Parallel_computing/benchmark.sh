3c27f2e20a0e600c77ac8e39c3485da89c9bcb5c
vsc37384 <vsc37384@tier2-p-login-2.genius.hpc.kuleuven.be>
2025-05-19 12:02:29 +0200

    should be done

--- Parallel_computing/benchmark.sh
+++ Parallel_computing/benchmark.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 echo "Running benchmarks..."
 
+
+# Run make clean to remove any existing output from previous runs
+# Run a make rule that generates a file test_time/test_serial_scored_snps.tsv
+# Wrap the make command in time to measure execution time
+# Redirects both stdout and stderr using 2>&1 | tee serial.time:
+# - 2>&1: merges error output with normal output
+# - serial.time: This file will contain the output of the time command, including the real time taken to execute the make command.
+# - tee: This command reads from standard input and writes to both standard output and the specified file.
 echo "1. Serial processing"
 make clean
-time make test_time/test_serial_scored_snps.tsv 2>&1 | tee serial.time
+mkdir -p test_time_report
+(time make test_time/test_serial_scored_snps.tsv) 2>&1 | tee test_time_report/serial.time
 
-echo "2. 10 parallel processes"
+echo "2. 4 parallel processes"
 make clean
-time make test_time/test_p10_scored_snps.tsv 2>&1 | tee p10.time
+mkdir -p test_time_report
+(time make test_time/test_p4_scored_snps.tsv) 2>&1 | tee test_time_report/p4.time
 
-echo "3. 25 parallel processes"
+echo "3. 8 parallel processes"
 make clean
-time make test_time/test_p25_scored_snps.tsv 2>&1 | tee p25.time
+mkdir -p test_time_report
+(time make test_time/test_p8_scored_snps.tsv) 2>&1 | tee test_time_report/p8.time
 
-echo "4. 50 parallel processes"
+echo "4. 16 parallel processes"
 make clean 
-time make test_time/test_p50_scored_snps.tsv 2>&1 | tee p50.time
+mkdir -p test_time_report
+(time make test_time/test_p16_scored_snps.tsv) 2>&1 | tee test_time_report/p16.time
 
-echo "5. 100 parallel processes"
+echo "5. 36 parallel processes"
 make clean
-time make test_time/test_p100_scored_snps.tsv 2>&1 | tee p100.time
+mkdir -p test_time_report
+(time make test_time/test_p36_scored_snps.tsv) 2>&1 | tee test_time_report/p36.time
